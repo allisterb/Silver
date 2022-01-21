b4c18eb4ad55069bb276d36063e79ac4c4f53edc
Allister Beharry <allister.beharry@gmail.com>
2022-01-21 13:43:24 -0400

    Format analyzer code, add release MDs.

--- src/Silver.CodeAnalysis.Vs/Silver.CodeAnalysis.Vs/SilverCodeAnalysisVsAnalyzer.cs
+++ src/Silver.CodeAnalysis.Vs/Silver.CodeAnalysis.Vs/SilverCodeAnalysisVsAnalyzer.cs
@@ -13,29 +13,18 @@ namespace Silver.CodeAnalysis.Vs
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class SilverCodeAnalysisVsAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "SilverCodeAnalysisVs";
-
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
-
-        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
-
+        #region Overriden members
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
         public override void Initialize(AnalysisContext context)
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
-
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
             context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
         }
+        #endregion
 
+        #region Methods
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
         {
             // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
@@ -50,5 +39,17 @@ namespace Silver.CodeAnalysis.Vs
                 context.ReportDiagnostic(diagnostic);
             }
         }
+        #endregion
+
+        #region Fields
+        public const string DiagnosticId = "SilverCodeAnalysisVs";
+        private const string Category = "Naming";
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        #endregion
     }
 }
