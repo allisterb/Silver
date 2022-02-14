79f3b815a94068e1d3110f0bcf3c326ada9e3c4a
Allister Beharry <allisterb@users.noreply.github.com>
2022-02-14 10:34:43 -0400

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,10 +1,6 @@
 @echo off
 @setlocal
-pushd
 set ERROR_CODE=0
-git fetch
-git pull --recurse-submodules
-build
+git fetch && git pull --recurse-submodules && build
 :end
-popd
 exit /B %ERROR_CODE%
