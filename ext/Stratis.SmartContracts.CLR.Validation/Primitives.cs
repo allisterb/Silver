ffa466d17556fe41f34592ea79b621a18208bb55
Allister Beharry <allister.beharry@gmail.com>
2022-03-04 20:12:25 -0400

    Add Stratis.SmartContracts.CLR.Validator as external project.

---
+++ ext/Stratis.SmartContracts.CLR.Validation/Primitives.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stratis.SmartContracts.CLR.Validation
+{
+    public static class Primitives
+    {
+        public static IEnumerable<Type> Types { get; } = new []
+        {
+            typeof(bool),
+            typeof(byte),
+            typeof(sbyte),
+            typeof(char),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(UInt128),
+            typeof(UInt256),
+            typeof(string)
+        };
+    }
+}
