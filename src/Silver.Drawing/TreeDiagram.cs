271bbb5d381846a25d4032093661a9be4836eb7b
Allister Beharry <allister.beharry@gmail.com>
2022-09-03 09:23:30 -0400

    Add TreeDiagram classes.

---
+++ src/Silver.Drawing/TreeDiagram.cs
@@ -0,0 +1,36 @@
+﻿namespace Silver.Drawing;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class TreeNode
+{
+    public string Data { get; set; }
+
+    public Dictionary<string, string> Attributes { get; set; }
+
+    public List<TreeNode> Children { get; set; }
+
+    public TreeNode(string data, Dictionary<string, string>? attributes =  null, List<TreeNode>? children = null)
+    {
+        Data = data;
+        Attributes = attributes ?? new Dictionary<string, string>();
+        Children = children ?? new List<TreeNode>();
+    }
+}
+
+public class TreeDiagram
+{
+    public List<TreeNode> TreeNodes { get; set;}
+
+    public TreeDiagram(IEnumerable<TreeNode> treeNodes)
+    {
+        TreeNodes = treeNodes.ToList();
+    }
+
+    public string DrawWithJSTree()
+    {
+
+    }
+}
+
---
+++ src/Silver.Notebooks/JSLibs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silver.Notebooks
+{
+    public class JSLibs
+    {
+        public static bool JQueryLoaded { get; set; } = false;
+    }
+}
